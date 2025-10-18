9ae70123498ee94c767871b363e16000a23e2794
hartun9 <h4rtun9@gmail.com>
2025-10-18 16:09:54 +0900

    feat: add ex03.c

---
+++ l2/ex03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * @brief 練習問題1
+ *
+ * @return 0
+ */
+int main(void)
+{
+  char initial = 'M';
+  int age = 42;
+  double height = 164.5;
+
+  printf("initial: %c\n", initial);
+  printf("age: %d\n", age);
+  printf("height: %f\n", height);
+
+  return 0;
+}
