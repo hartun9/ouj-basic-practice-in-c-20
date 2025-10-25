d3b110086e97b66b8db4ddd142187971b3622e11
hartun9 <h4rtun9@gmail.com>
2025-10-25 12:01:37 +0900

    feat: add height

--- l2/ex12.c
+++ l2/ex12.c
@@ -4,13 +4,16 @@ int main(void)
 {
   char initial;
   int age;
+  double height;
 
   printf("名字の頭文字を入力してください > ");
   scanf("%c", &initial);
   printf("年齢を入力してください > ");
   scanf("%d", &age);
+  printf("身長を入力してください > ");
+  scanf("%lf", &height);
 
-  printf("%cさんの年齢は%d歳です。\n", initial, age);
+  printf("%cさんの年齢は%d歳、身長は%.1fcmです。\n", initial, age, height);
 
   return 0;
 }
